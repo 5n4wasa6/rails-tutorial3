33c2b47e7d30930e8ae078eacf2698640246284d
shingo <kurosawa1991@gmail.com>
2020-01-30 13:34:01 +0900

    add ~13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -46,6 +47,12 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -53,16 +60,6 @@ class UsersController < ApplicationController
     end
 
     # beforeアクション
-
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
